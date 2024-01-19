5bba79e3fe790d9ca85c0dba5c0ef648bcd4b597
Nick Robison <nick@nickrobison.com>
2024-01-19 13:51:57 -0800

    Add cohttp_mock library

---
+++ cohttp_mock/client.mli
@@ -0,0 +1,20 @@
+(*Proudly stolen from: https://github.com/ghuysmans/api-cohttp-lwt*)
+
+include
+  Cohttp_lwt.S.Client
+    with type ctx =
+      ?headers:Cohttp.Header.t ->
+      ?body:string ->
+      ?chunked:bool ->
+      Cohttp.Code.meth ->
+      Uri.t ->
+      (Cohttp.Response.t * Cohttp_lwt.Body.t) Lwt.t
+
+type router =
+  (?headers:Cohttp.Header.t ->
+  ?body:string ->
+  Cohttp.Code.meth ->
+  (Cohttp.Code.status_code * string) Lwt.t)
+  Routes.router
+
+val ctx_of_router : router -> ctx
